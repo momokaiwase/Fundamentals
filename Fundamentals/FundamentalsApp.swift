25e565efd821d5243492a0b59a4fae2b8b38064e
iwasemo <iwasemo@bc.edu>
2025-02-02 17:11:48 -0500

    Initial Commit

---
+++ Fundamentals/FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundamentalsApp.swift
+//  Fundamentals
+//
+//  Created by Momoka Iwase on 2025/02/02.
+//
+
+import SwiftUI
+
+@main
+struct FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
